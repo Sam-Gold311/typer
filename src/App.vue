3e41181d79980bc1647b6ea3d263c17f9407cf18
Tagir <sulaev.tagir@mail.ru>
2021-03-31 21:37:48 +0400

    add key component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="text-h4 d-flex align-self-center">
+        Typer
+      </div>
+
+    </v-app-bar>
+
+    <v-main>
+      <div class='typer d-flex flex-column align-center'>
+        <div class="typer__title text-h1 animate__animated animate__fadeInLeftBig">
+          Welcome to the Typer
+        </div>
+        <div class="typer__subtitle text-h2 animate__animated animate__fadeInRightBig animate__delay-1s">
+          Show your speed!
+        </div>
+        
+        <div class="typer__keyboard animate__animated animate__fadeIn animate__delay-2s animate__slow">
+          <Key ref='keys' v-for='(key, i) in enVersion.keys' :key='i' :somekey='key'/>
+        </div>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Key from '@/components/Key.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Key
+  },
+  data: () => ({
+    enVersion: {
+      keys: [
+        { keyText: 'q', code: 'KeyQ' },
+        { keyText: 'w', code: 'KeyW' },
+      ]
+    }
+  }),
+  mounted() {
+    document.addEventListener('keydown', this.keyPressed);
+  },
+  methods: {
+    keyPressed(e) {
+      for (let i = 0; i < this.enVersion.keys.length; i++) {
+        if(this.$refs.keys[i].somekey.code === e.code) {
+          this.$refs.keys[i].keyPressed();
+          return;
+        }
+      }
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style media="screen" lang='scss'>
+
+  .typer {
+    &__title {
+      margin: 25px 0;
+    }
+    &__subtitle {
+      margin-bottom: 50px;
+    }
+    &__area {
+      width: 60%;
+    }
+    &__keyboard {
+      display: flex;
+      padding: 10px;
+      border-radius: 10px;
+      border: 2px solid #000;
+      width: 40%;
+      height: 300px;
+      font-weight: bold;
+      font-size: 20px;
+    }
+  }
 </style>
---
+++ src/components/Key.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class='key' v-ripple="{ class: 'success--text' }" >
+    {{ somekey.keyText }}
+  </div>
+</template>
+
+
+<script>
+
+  export default {
+    name: 'Key',
+    props: {
+      somekey: Object
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+      keyPressed() {
+        this.$el.mousedown();
+      }
+    }
+  }
+</script>
+
+
+
+
+
+<style media="screen" lang='scss'>
+  .key {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 50px;
+    height: 50px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+</style>
